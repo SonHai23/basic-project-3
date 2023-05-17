52356db4afb14fc3b00c5bde2a26df169cea3a8a
trinhdamhuy@gmail.com <trinhdamhuy@gmail.com>
2023-05-18 00:54:40 +0700

    xong phần search

--- app/src/main/java/com/example/basicproject3/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/basicproject3/ui/fragments/SearchFragment.kt
@@ -1,64 +1,119 @@
 package com.example.basicproject3.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.example.basicproject3.data.model.Event
 import com.example.basicproject3.databinding.FragmentSearchBinding
 import com.example.basicproject3.ui.adapters.CategoryListAdapter
+import com.example.basicproject3.ui.adapters.EventListAdapter
 import com.example.basicproject3.ui.viewmodels.SearchViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
-
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var db = Firebase.firestore
     private val binding get() = _binding!!
+    private val events: MutableList<Event> = mutableListOf()
+    private val filteredEvents: MutableList<Event> = mutableListOf()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val searchViewModel =
-            ViewModelProvider(this)[SearchViewModel::class.java]
+        val searchViewModel = ViewModelProvider(this)[SearchViewModel::class.java]
 
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
-
 
         //load category list
         lifecycleScope.launch {
             binding.progressBarCategory.visibility = View.VISIBLE
             val categoryList = searchViewModel.getCategoryList()
-            val recyclerView = binding.recyclerViewCategories
-//            val eventList = searchViewModel.getEventsByName()
-            recyclerView.adapter = CategoryListAdapter(categoryList)
-//            binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//                override fun onQueryTextSubmit(query: String?): Boolean {
-//                    binding.searchBar.clearFocus()
-//                    if (eventList != null) {
-//                        binding.recyclerViewEvents.adapter = EventListAdapter(activity, )
-//                    }
-//                }
-//
-//                override fun onQueryTextChange(p0: String?): Boolean {
-//                    TODO("Not yet implemented")
-//                }
-//
-//
-//            })
+            val recyclerViewCategory = binding.recyclerViewCategories
+            recyclerViewCategory.adapter = CategoryListAdapter(categoryList)
             binding.progressBarCategory.visibility = View.GONE
         }
+
+        context?.let { onSearchListener(it) }
         //Clear focus from the search bar when clicked outside
-        binding.searchPage.setOnClickListener { binding.searchBar.clearFocus() }
+        binding.searchPage.setOnClickListener {
+            binding.searchBar.clearFocus()
+            clearFilteredEventsAndAdapter()
+        }
+
+        loadEvents()
 
         return binding.root
+    }
+
+    private fun onSearchListener(context: Context) {
+
+        binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    filterEvents(newText)
+                    binding.recyclerViewEvents.adapter = EventListAdapter(context, filteredEvents)
+                } else {
+                    clearFilteredEventsAndAdapter()
+                }
+                return true
+            }
+        })
+    }
+
+    private fun loadEvents() {
+        val collectionRef = db.collection("events")
+        collectionRef.get().addOnSuccessListener { querySnapshot ->
+            events.clear()
+            for (document in querySnapshot) {
+                val book = document.toObject(Event::class.java)
+                events.add(book)
+            }
+            filterEvents("")
+        }
+    }
+
+    private fun clearFilteredEventsAndAdapter() {
+        filteredEvents.clear()
+        binding.recyclerViewEvents.adapter = EventListAdapter(requireContext(), filteredEvents)
+    }
+
+//    fun reloadEventList(context: Context, query: String?) {
+//        val collectionRef = db.collection("events")
+//        if (query != null) {
+//            collectionRef.whereGreaterThanOrEqualTo("title", query)
+//                .whereLessThanOrEqualTo("title", query + "\uF7FF").get().addOnSuccessListener {
+//                    val eventList = mutableListOf<Event>()
+//                    for (document in it) {
+//                        eventList.add(document.toObject(Event::class.java))
+//                    }
+//                    binding.recyclerViewEvents.adapter = EventListAdapter(context, eventList)
+//                }
+//        }
+//    }
+
+    private fun filterEvents(query: String) {
+        filteredEvents.clear()
+
+        if (query.isEmpty()) {
+            filteredEvents.addAll(events)
+        } else {
+            for (event in events) if (event.title!!.lowercase().contains(query.lowercase())) {
+                filteredEvents.add(event)
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/basicproject3/ui/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/example/basicproject3/ui/viewmodels/SearchViewModel.kt
@@ -3,6 +3,7 @@ package com.example.basicproject3.ui.viewmodels
 import androidx.lifecycle.ViewModel
 import com.example.basicproject3.data.model.Category
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
@@ -14,19 +15,9 @@ class SearchViewModel : ViewModel() {
         val categoryList = mutableListOf<Category>()
         val querySnapshot = db.collection("categories").get().await()
         for (document in querySnapshot) {
-//            val category = document.toObject<Category>()
-//            categoryList.add(category)
-            categoryList.add(Category(document.data["name"] as String))
+            val category = document.toObject<Category>()
+            categoryList.add(category)
         }
         return categoryList
     }
-
-//    suspend fun getEventsByName(): MutableList<Event> {
-//        val eventList = mutableListOf<Event>()
-//        val querySnapshot = db.collectionGroup("events").get().await()
-//        for (document in querySnapshot) {
-//            eventList.add(Event(id = document.id, title = document.data["title"] as String?))
-//        }
-//        return eventList
-//    }
 }
