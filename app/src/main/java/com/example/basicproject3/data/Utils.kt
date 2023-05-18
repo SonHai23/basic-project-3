4bf4d80f1a27e337727b236d79b4b2e87cb3a265
trinhdamhuy@gmail.com <trinhdamhuy@gmail.com>
2023-05-18 10:16:45 +0700

    create public method for format date to string

---
+++ app/src/main/java/com/example/basicproject3/data/Utils.kt
@@ -0,0 +1,14 @@
+package com.example.basicproject3.data
+
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Utils {
+    companion object {
+        fun formatDate(date: Timestamp?): String? {
+            val formatter = SimpleDateFormat("MMMM dd, yyyy 'at' HH:mm z", Locale.getDefault())
+            return formatter.format(date!!.toDate())
+        }
+    }
+}
